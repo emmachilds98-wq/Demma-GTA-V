cf0e7f468af753dfbf285e957d868a5736c7e640
emmachilds98-wq <emma.childs98@gmail.com>
2026-08-31 21:29:42 +0100

    Cache the mobile app image for offline use

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "demma-gta-ops-v1";
-const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/los-santos-ops-hero.png"];
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/los-santos-ops-hero.jpg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
