4a1bec6ff14806c7fbdf4016322a8867f54cf7ee
emmachilds98-wq <emma.childs98@gmail.com>
2026-09-01 01:25:08 +0100

    Refresh cache for jobs and heists planning

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "demma-gta-ops-v26";
+const CACHE_NAME = "demma-gta-ops-v27";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/los-santos-ops-hero.jpg", "./assets/demma-ops-icon-180.png"];
 
 self.addEventListener("install", event => {
